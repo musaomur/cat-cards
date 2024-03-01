a01c2ab3792eddfa794fb9fba7e0195b8c4411f9
Jennifer Smale <JenniferSmale7@gmail.com>
2024-03-01 13:16:40 -0500

    completed team project

--- java/src/main/java/com/techelevator/controller/CatController.java
+++ java/src/main/java/com/techelevator/controller/CatController.java
@@ -5,10 +5,14 @@ import com.techelevator.model.CatCard;
 import com.techelevator.model.CatFact;
 import com.techelevator.services.CatFactService;
 import com.techelevator.services.CatPicService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping(path = "/api/cards/")
+@RequestMapping(path = "/api/cards")
 public class CatController {
 
     private CatCardDao catCardDao;
@@ -37,12 +41,30 @@ public class CatController {
         return card;
     }
 
+    @GetMapping
+    public List<CatCard> allCards() {
+        List<CatCard> cardList = new ArrayList<>();
+        cardList = catCardDao.getCatCards();
+        return cardList;
+    }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void addCardToCollection(@RequestBody CatCard catCard) {
+        catCardDao.createCatCard(catCard);
+    }
 
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void updateCard(@RequestBody CatCard catCard, @PathVariable int id) {
+        catCard.setCatCardId(id);
+        catCardDao.updateCatCard(catCard);
+    }
 
-
-
-
-
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCard(@PathVariable int id) {
+        catCardDao.deleteCatCardById(id);
+    }
 
 }
